6133d17a309f8c216767de00d67b1f0a8ae27d2a
Haichen Shen <haichen@cs.washington.edu>
2017-07-06 10:17:46 -0700

    Support customize CUDA path and Tensorflow version; add pkgconfig in the cmake; disable compile and install static libprotobuf

--- cmake/TensorflowShared.cmake
+++ cmake/TensorflowShared.cmake
@@ -9,10 +9,10 @@ ExternalProject_Add(
   BUILD_IN_SOURCE 1
   DOWNLOAD_COMMAND ""
   UPDATE_COMMAND ""
-  CONFIGURE_COMMAND tensorflow/contrib/makefile/compile_linux_protobuf.sh
-            COMMAND cp "${CMAKE_CURRENT_SOURCE_DIR}/cmake/build_tensorflow.sh" .
+  CONFIGURE_COMMAND cp "${CMAKE_CURRENT_SOURCE_DIR}/cmake/build_tensorflow.sh" .
             COMMAND cp "${CMAKE_CURRENT_SOURCE_DIR}/cmake/copy_links.sh" .
-            COMMAND ./build_tensorflow.sh
+            #COMMAND tensorflow/contrib/makefile/compile_linux_protobuf.sh
+            COMMAND env CUDA_PATH=${CUDA_PATH} CUDNN_PATH=${CUDNN_PATH} ./build_tensorflow.sh
             COMMAND ./copy_links.sh .
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
